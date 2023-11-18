99285672242ca76ebeef887c787464124c5002ee
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-11-18 17:11:28 +0200

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/Root';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import MovieListing from './components/MovieListing';
+import MovieDetail from './components/MovieDetail';
+// Custom container component
+const CustomContainer = ({ children }) => (
+  <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '0 15px' }}>
+    {children}
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/movie/:imdbID',
+        element: <MovieListing />,
+        children: [
+          {
+            path: '/movie/:imdbID',
+            element: <MovieDetail />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+const App = () => (
+  <CustomContainer>
+    <RouterProvider router={router} />
+  </CustomContainer>
+);
 
 export default App;
